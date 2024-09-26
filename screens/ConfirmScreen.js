69fae401751b5483ca7f8d3ef2023ec9165c1a06
Spec-DY <ibonehp@gmail.com>
2024-09-26 15:03:48 -0700

    change confirm to use Card

--- screens/ConfirmScreen.js
+++ screens/ConfirmScreen.js
@@ -3,6 +3,7 @@ import { View, Text, Modal, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import CustomButton from '../components/CustomButton';
 import color from '../color'
+import Card from '../components/Card'
 
 const ConfirmScreen = ({ visible, name, email, phone, isCheckboxChecked, onConfirm, onGoBack }) => {
   return (
@@ -13,21 +14,21 @@ const ConfirmScreen = ({ visible, name, email, phone, isCheckboxChecked, onConfi
       <LinearGradient
         colors={[color.gradientColor, 'rgba(0,0,0,0)']}
         style={styles.gradientBackground}>
-        <View style={styles.container}>
-            <Text style={styles.title}>Hello {name}</Text>
-            <Text style={styles.text}>You Entered:</Text>
-            <Text style={styles.text}>Email Address: {email}</Text>
-            <Text style={styles.text}>Phone Number: {phone}</Text>
-            <Text style={styles.text}>Privacy Terms Accepted: {isCheckboxChecked ? 'Yes' : 'No'}</Text>
-            <Text style={styles.text}>
-                Please Click Go Back If Incorrect
-            </Text>
+        <Card>
+          <Text style={styles.title}>Hello {name}</Text>
+              <Text style={styles.text}>You Entered:</Text>
+              <Text style={styles.text}>Email Address: {email}</Text>
+              <Text style={styles.text}>Phone Number: {phone}</Text>
+              <Text style={styles.text}>Privacy Terms Accepted: {isCheckboxChecked ? 'Yes' : 'No'}</Text>
+              <Text style={styles.text}>
+                  Please click Go Back if incorrect
+              </Text>
 
-            <View style={styles.buttonContainer}>
-                <CustomButton title="Go Back" onPress={onGoBack} color="red" />
-                <CustomButton title="Continue" onPress={onConfirm} color="blue" />
-            </View>
-        </View>
+              <View style={styles.buttonContainer}>
+                  <CustomButton title="Go Back" onPress={onGoBack} color="red" />
+                  <CustomButton title="Continue" onPress={onConfirm} color="blue" />
+              </View>
+        </Card>
       </LinearGradient>
     </Modal>
   );
@@ -38,21 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  container: {
-    width: '80%',
-    padding: 20,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor:'white',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
   title: {
     fontSize: 20,
